3911752343337032a7dd253162a4e7490460c6aa
Akhmed Sakip <akhmedsakip@gmail.com>
2024-03-21 20:27:32 +0000

    OPT script: corrected max local_bsz_bound

--- examples/opt_finetune/run_clm_flax_adaptive.py
+++ examples/opt_finetune/run_clm_flax_adaptive.py
@@ -89,7 +89,7 @@ import datetime
 import wandb
 
 # alpa.init(cluster="ray")
-# alpa.init(cluster="ray", num_nodes=1, num_devices_per_node=2, namespace="alpa_default_space_non-adaptive_bert")
+# alpa.init(cluster="ray", num_nodes=1, num_devices_per_node=1, namespace="alpa_default_space_non-adaptive_opt")
 alpa.init(cluster="ray", scheduler_address="http://127.0.0.1:8000")
 
 from transformers.testing_utils import CaptureLogger
@@ -802,7 +802,7 @@ def main():
     )
 
     train_loader.autoscale_batch_size(max_batch_size = 400, 
-                                    local_bsz_bounds=(train_batch_size // alpa.get_global_num_devices(), 100), gradient_accumulation=False)
+                                    local_bsz_bounds=(train_batch_size // alpa.get_global_num_devices(), 78), gradient_accumulation=False)
 
     eval_loader = DataLoader(
         eval_dataset,
